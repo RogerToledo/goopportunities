bb53273bdc6cb303ecbbe2b31f5bc8c3097613c1
Roger Toledo <rogertoledo28@gmail.com>
2024-02-23 09:23:22 -0300

    refactor: add logic to show and update

--- handler/opening/show.go
+++ handler/opening/show.go
@@ -2,12 +2,28 @@ package opening
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/me/goopportunities/config"
+	"github.com/me/goopportunities/handler"
+	"github.com/me/goopportunities/schemas"
 )
 
 func ShowOpening(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "GET Opening",
-	})
+	db := config.GetSQLite()
+	opening := schemas.Opening{}
+	id := ctx.Query("id")
+
+	if err := db.First(&opening, id).Error; err != nil {
+		if strings.Contains(err.Error(), "not found") {
+			handler.SendError(ctx, http.StatusNotFound, "opening not found")
+			return
+		} else {
+			handler.SendError(ctx, http.StatusInternalServerError, err.Error())
+			return
+		}	
+	}
+
+	handler.SendSuccess(ctx, "opening found", opening)
 }
--- handler/opening/update.go
+++ handler/opening/update.go
@@ -2,12 +2,64 @@ package opening
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/me/goopportunities/config"
+	"github.com/me/goopportunities/handler"
+	"github.com/me/goopportunities/schemas"
 )
 
 func UpdateOpening(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "PUT Opening",
-	})
+	db := config.GetSQLite()
+	opening := schemas.Opening{}
+
+	request := handler.UpsertOpeningRequest{}
+	ctx.BindJSON(&request)
+
+	if err := request.ValidateUpdate(); err != nil {
+		handler.SendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+	
+	id := ctx.Query("id")
+
+	if id == "" {
+		handler.SendError(ctx, http.StatusBadRequest, "id is required")
+		return
+	}
+
+	if err := db.First(&opening, id).Error; err != nil {
+		if strings.Contains(err.Error(), "not found") {
+			handler.SendError(ctx, http.StatusNotFound, "opening not found")
+			return
+		} else {
+			handler.SendError(ctx, http.StatusInternalServerError, err.Error())
+			return
+		}	
+	}
+
+	if request.Role != "" {
+		opening.Role = request.Role
+	}
+	if request.Company != "" {
+		opening.Company = request.Company
+	}
+	if request.Location != "" {
+		opening.Location = request.Location
+	}
+	if request.Remote != nil {
+		opening.Remote = *request.Remote
+	}
+	if request.Link != "" {
+		opening.Link = request.Link
+	}
+	if request.Salary > 0 {
+		opening.Salary = request.Salary
+	}
+
+	if err := db.Save(opening).Error; err != nil {
+		handler.SendError(ctx, http.StatusInternalServerError, err.Error())
+		return
+	}
 }
