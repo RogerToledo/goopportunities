2cb94a2476d9a4072dbaefea1f96f3accb3494d7
Roger Toledo <rogertoledo28@gmail.com>
2024-02-23 09:22:48 -0300

    refactor: add list logigic

--- handler/opening/list.go
+++ handler/opening/list.go
@@ -4,10 +4,25 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/me/goopportunities/config"
+	"github.com/me/goopportunities/handler"
+	"github.com/me/goopportunities/schemas"
 )
 
 func ListOpening(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "List Opening",
-	})
+	db := config.GetSQLite()
+	openings := []schemas.Opening{}
+
+	if err := db.Find(&openings).Error; err != nil {
+		handler.SendError(ctx, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	if len(openings) == 0 {
+		handler.SendError(ctx, http.StatusNotFound, "No openings found")
+		return
+	}
+
+	handler.SendSuccess(ctx, "openings find", openings)
+
 }
